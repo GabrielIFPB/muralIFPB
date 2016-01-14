6cee03d95d329a2e9b92be5e3f132c2de61951cc
GabrielIFPB <jeronimogbariel@gmail.com>
2016-01-14 19:42:20 -0300

    usando from django.contrib.auth.models import

--- muralifpb/models.py
+++ muralifpb/models.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
 class Contact(models.Model):
 	name = models.CharField(max_length=100)
@@ -47,3 +48,7 @@ class Login(models.Model):
 
 	def __unicode__(self):
 		return self.login
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User, related_name='user_profile')
+	pass
